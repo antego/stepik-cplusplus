878c6564144d36a3661307608f72d09878ba68f3
antego <antego@users.noreply.github.com>
2017-08-12 23:39:04 +0300

    Posix queue

--- chapter6/main.cpp
+++ chapter6/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/msg.h>
+//#include <sys/msg.h>
 #include <fstream>
+#include <string.h>
 
 using namespace std;
 
@@ -17,15 +18,15 @@ int main()
     if((key = ftok("/tmp/msg.temp", 1))==-1)
         perror("ftok");
     int msgid;
-    if((msgid = msgget(key, 0660 | IPC_CREAT ))==-1)
+    if((msgid = msgget(key, 0660 | IPC_CREAT))==-1)
         perror("msgget");
-    message* msg;
-    if(msgrcv(msgid, (void*)msg, 80, 0, 0)==-1)
+    message msg;
+    if(msgrcv(msgid, (void*)&msg, 80, 0, 0)==-1)
         perror("msgrcv()");
     cout << "Hello World!" << endl;
     ofstream ofs;
     ofs.open("/home/box/message.txt");
-    ofs.write((msg->mtext), 80);
+    ofs.write((msg.mtext), strlen(msg.mtext));
     ofs.close();
     if(msgctl(msgid, IPC_RMID, NULL)==-1)
         perror("rm queue");
---
+++ chapter6/posixmq.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sys/types.h>
+#include <fstream>
+#include <string.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
+
+using namespace std;
+
+int main()
+{
+    mqd_t id;
+    if ((id = mq_open("/test.mq", O_RDONLY | O_CREAT) == -1))
+        perror("mq_open");
+    char msg[80];
+    if (mq_receive(id, msg, 80, NULL) == -1)
+        perror("mq_receive");
+    cout << "Hello World!" << endl;
+    ofstream ofs;
+    ofs.open("/home/box/message.txt");
+    ofs.write((msg), strlen(msg));
+    ofs.close();
+    if(mq_close(id) == -1)
+        perror("mq_close");
+    return 0;
+}
